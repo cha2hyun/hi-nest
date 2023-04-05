acdd545361c0420b6be6feb2cb1eb68a60c9cf83
cha2hyun <cha2hyun.dev@gmail.com>
2023-04-05 11:30:13 +0900

    #2.0 Movies Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [MoviesController],
+  providers: [],
 })
 export class AppModule {}
 
 /*
+#1
 모듈을 장고로 치면 앱
 컨트롤러 : url가져오고 함수를 실행함 (Express 의 라우터)
 서비스 : 실제로 function을 가지는 부분 컨트롤러에서 함수를 실제로 실행하는 곳 비지니스 로직을 실행하는 것
+
+#2
+nest cli
+컨트롤러 생성 : nest g co (자동으로 앱 모듈에 임포트됨)
 */
