7179ecef809e52a119693c13b89c161e3062a187
cha2hyun <cha2hyun.dev@gmail.com>
2023-04-05 11:17:48 +0900

    #1.2 Services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('/hello')
+  @Get('/hello')
   sayHello(): string {
-    return 'Hello Everyone';
+    return this.appService.getHi();
   }
 }
